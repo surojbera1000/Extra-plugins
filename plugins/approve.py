fc55cd950b5819743cff6f644c31f018ce2eb67a
MISH0009 <mishuxd.19@gmail.com>
2024-10-06 20:11:57 +0530

    Update approve.py

--- plugins/approve.py
+++ plugins/approve.py
@@ -13,9 +13,9 @@ from pyrogram.enums import ChatMembersFilter
 from pyrogram.types import ChatJoinRequest
 from pyrogram.errors.exceptions.bad_request_400 import UserAlreadyParticipant
 from DnsXMusic import app
-from YukkiMusic.core.mongo import mongodb
-from YukkiMusic.misc import SUDOERS
-from YukkiMusic.utils.keyboard import ikb
+from DnsXMusic.core.mongo import mongodb
+from DnsXMusic.misc import SUDOERS
+from DnsXMusic.utils.keyboard import ikb
 from utils.permissions import adminsOnly, member_permissions
 
 approvaldb = mongodb.autoapprove
